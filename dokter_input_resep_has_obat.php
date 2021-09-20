ed8f6c29cc727cc0f08dee7cff4f98d2bc142c1f
mlatifr <mlatifraldi@gmail.com>
2021-09-20 14:42:09 +0700

    dktr inpt Rsp_h_obt 1 obt 1 rsp: hrs 1rsp byk obt

--- dokter_input_resep_has_obat.php
+++ dokter_input_resep_has_obat.php
@@ -3,14 +3,15 @@
 <?php
 // $visit_id;
 extract($_POST);
-$sql = "INSERT INTO `resep_has_obat` (`visit_id`,`obat_id`) VALUES (?,?)";
+echo $resep_id . ' ' . $obat_id . ' ' . $dosis;
+$sql = "INSERT INTO `resep_has_obat` (`resep_id`, `obat_id`,`dosis`) VALUES (?,?,?)";
 $stmt = $con->prepare($sql);
-$stmt->bind_param("ss", $visit_id, $obat_id);
+$stmt->bind_param("sss", $resep_id, $obat_id, $dosis);
 $stmt->execute();
 if ($stmt->affected_rows > 0) {
     $visit_id = $con->insert_id;
     // echo $visit_id . ' ' . $user_id;
-    $arr = ["result" => "success", "resep_id" => $con->insert_id];
+    $arr = ["result" => "success", "resep_has_obat_id" => $con->insert_id];
 } else {
     $arr = ["result" => "fail", "visit_id" => $visit_id, "Error" => $con->error];
 }
