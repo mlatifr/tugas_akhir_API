92cfe762da5562f2e1674dd1860d75e1cf4b04a7
mlatifr <mlatifraldi@gmail.com>
2021-09-21 18:17:56 +0700

    rspHasObat bisa echo array jdl array: rspObatDosis

--- dokter_input_resep_has_obat_array.php
+++ dokter_input_resep_has_obat_array.php
@@ -3,45 +3,33 @@
 <?php
 
 $obat_array = array(
-    '0' => array('4', '7', '000'),
-    '1' => array('4', '7', '111'),
-    '2' => array('4', '7', '222'),
-    '3' => array('4', '7', '333'),
+    'obat1' => array('4', '7', '000'),
+    'obat2' => array('4', '7', '111'),
+    // '2' => array('4', '7', '222'),
+    // '3' => array('4', '7', '333'),
+    // '4' => array('4', '7', '444'),
 );
 foreach ($obat_array as $row => $value) {
     $resep_id = mysqli_real_escape_string($con, $value[0]);
     $obat_id = mysqli_real_escape_string($con, $value[1]);
     $dosis = mysqli_real_escape_string($con, $value[2]);
     $sql = "INSERT INTO resep_has_obat(`resep_id`, `obat_id`,`dosis`) VALUES ('" . $resep_id . "', '" . $obat_id . "', '" . $dosis . "')";
-    mysqli_query($con, $sql);
+    // mysqli_query($con, $sql);
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
 };
-// $visit_id;
-// extract($_POST);
-
-// $jumlah_data = 5;
-// $angka_obat = 0;
-// for ($i = 0; $i <= $jumlah_data; $i++) {
-//     $angka_obat++;
-//     $obat = 'obat' . $angka_obat;
-//     $dosis = $i . 'x1';
-//     // echo ("INSERT INTO `resep_has_obat` (`resep_id`, `obat_id`,`dosis`) VALUES (4,7,$dosis)");
-//     $sql = "INSERT INTO `resep_has_obat` (`resep_id`, `obat_id`,`dosis`) VALUES (4,7,$dosis)";
-//     $stmt = $con->prepare($sql);
-//     $stmt->execute();
-// }
-// echo $resep_id . ' ' . $obat_id . ' ' . $dosis;
-// $sql = "INSERT INTO `resep_has_obat` (`resep_id`, `obat_id`,`dosis`) VALUES (?,?,?)";
-// $stmt = $con->prepare($sql);
-// $stmt->bind_param("sss", $resep_id, $obat_id, $dosis);
-// $stmt->execute();
-// if ($stmt->affected_rows > 0) {
-//     $visit_id = $con->insert_id;
-//     // echo $visit_id . ' ' . $user_id;
-//     $arr = ["result" => "success", "resep_has_obat_id" => $con->insert_id];
-// } else {
-//     $arr = ["result" => "fail", "visit_id" => $visit_id, "Error" => $con->error];
-// }
-// echo json_encode($arr);
+$arr = ["result" => "success"];
+if ($stmt->affected_rows > 0) {
+    foreach ($obat_array as $row => $value) {
+        // array_push($arr,  $value[0], $value[1], $value[2],);
+        $arr += ["resep_id$row" => $value[0], "obat_id$row" => $value[1], "dosis$row" => $value[2]];
+        // $arr += ['obat_id' => $value[1]];
+        // $arr += ['dosis' => $value[2]];
+    }
+} else {
+    $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
+}
+echo json_encode($arr);
 
 $con->close();
 
