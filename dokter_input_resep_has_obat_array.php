96f0f2d2e73227fbec0e04dfb601d4af63f83c91
mlatifr <mlatifraldi@gmail.com>
2021-09-22 06:38:21 +0700

    dokter dpt input bnyk obt dlm 1 resep: sukses

--- dokter_input_resep_has_obat_array.php
+++ dokter_input_resep_has_obat_array.php
@@ -3,11 +3,11 @@
 <?php
 
 $obat_array = array(
-    'obat1' => array('4', '7', '000'),
-    'obat2' => array('4', '7', '111'),
-    // '2' => array('4', '7', '222'),
-    // '3' => array('4', '7', '333'),
-    // '4' => array('4', '7', '444'),
+    'obat0' => array('4', '7', '000'),
+    'obat1' => array('4', '7', '111'),
+    'obat2' => array('4', '7', '222'),
+    'obat3' => array('4', '7', '333'),
+    'obat4' => array('4', '7', '444'),
 );
 foreach ($obat_array as $row => $value) {
     $resep_id = mysqli_real_escape_string($con, $value[0]);
@@ -18,14 +18,15 @@ foreach ($obat_array as $row => $value) {
     $stmt = $con->prepare($sql);
     $stmt->execute();
 };
-$arr = ["result" => "success"];
+$arr = [];
+$data = [];
 if ($stmt->affected_rows > 0) {
     foreach ($obat_array as $row => $value) {
         // array_push($arr,  $value[0], $value[1], $value[2],);
-        $arr += ["resep_id$row" => $value[0], "obat_id$row" => $value[1], "dosis$row" => $value[2]];
-        // $arr += ['obat_id' => $value[1]];
-        // $arr += ['dosis' => $value[2]];
+        // array_push($data,  $value[0], $value[1], $value[2],);
+        $data += ["resep_id_$row" => $value[0], "obat_id_$row" => $value[1], "dosis_$row" => $value[2],];
     }
+    $arr = ["result" => "success", "data" => $data];
 } else {
     $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
 }
