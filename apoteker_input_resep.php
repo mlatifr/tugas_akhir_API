52676e8f673118f0b20411697968f9dd2b572757
mlatifr <mlatifraldi@gmail.com>
2021-09-23 09:08:16 +0700

    apoteker inpt rsp_hs_obt

---
+++ apoteker_input_resep.php
@@ -0,0 +1,44 @@
+<?php require 'connect.php';
+?>
+<?php
+
+$obat_array = array(
+    'obat0' => array('1', '4', '7', '000'),
+    'obat1' => array('1', '4', '7', '111'),
+    'obat2' => array('1', '4', '7', '222'),
+    'obat3' => array('1', '4', '7', '333'),
+    'obat4' => array('1', '4', '7', '444'),
+);
+foreach ($obat_array as $row => $value) {
+    $apoteker_id = mysqli_real_escape_string($con, $value[0]);
+    $resep_id = mysqli_real_escape_string($con, $value[1]);
+    $obat_id = mysqli_real_escape_string($con, $value[2]);
+    $dosis = mysqli_real_escape_string($con, $value[3]);
+    $sql = "INSERT INTO resep_apoteker(`apoteker_id`,`resep_id`, `obat_id`,`dosis`) 
+    VALUES ('" . $apoteker_id . "','" . $resep_id . "', '" . $obat_id . "', '" . $dosis . "')";
+    // mysqli_query($con, $sql);
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+};
+$arr = [];
+$data = [];
+if ($stmt->affected_rows > 0) {
+    foreach ($obat_array as $row => $value) {
+        // array_push($arr,  $value[0], $value[1], $value[2],);
+        // array_push($data,  $value[0], $value[1], $value[2],);
+        $data += [
+            "apoteker_id_$row" => $value[0],
+            "resep_id_$row" => $value[1],
+            "obat_id_$row" => $value[2],
+            "dosis_$row" => $value[3],
+        ];
+    }
+    $arr = ["result" => "success", "data" => $data];
+} else {
+    $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
+}
+echo json_encode($arr);
+
+$con->close();
+
+?>
