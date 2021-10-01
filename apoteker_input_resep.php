307d5b6dc7814f735d26290e11f96f9db04ad83e
mlatifr <mlatifraldi@gmail.com>
2021-10-02 06:09:57 +0700

    aptkr: php(1): membuat resep php(2):memasukan obat

--- apoteker_input_resep.php
+++ apoteker_input_resep.php
@@ -2,17 +2,13 @@
 ?>
 <?php
 $arr = [];
-$data = [];
+$last_id;
 extract($_POST);
 foreach ($obat_array as $row => $value) {
-
     $visit_id = mysqli_real_escape_string($con, $value['visit_id']);
     $nama_pembeli = mysqli_real_escape_string($con, $value['nama_pembeli']);
     $user_id_apoteker = mysqli_real_escape_string($con, $value['user_id_apoteker']);
     $tgl_penulisan_resep = mysqli_real_escape_string($con, $value['tgl_penulisan_resep']);
-    $obat_id = mysqli_real_escape_string($con, $value['obat_id']);
-    $jumlah = mysqli_real_escape_string($con, $value['jumlah']);
-    $dosis = mysqli_real_escape_string($con, $value['dosis']);
     //pelayanan resep dari pasien yg visit
     if ($visit_id) {
         $sql = "INSERT INTO `resep_apoteker` ( `visit_id`, `nama_pembeli`, `user_id_apoteker`, `tgl_penulisan_resep`)
@@ -20,19 +16,8 @@ foreach ($obat_array as $row => $value) {
         $stmt = $con->prepare($sql);
         $stmt->execute();
         if ($stmt->affected_rows > 0) {
-            echo 'sql 1 success' . "\n";
             $last_id = $con->insert_id;
-            // INSERT INTO `rsp_aptkr_has_obat` (`resep_apoteker_id`, `obat_id`, `jumlah`, `dosis`) VALUES ('1', '7', '30', '3x1')
-            $sql2 = "INSERT INTO `rsp_aptkr_has_obat` (`resep_apoteker_id`, `obat_id`, `jumlah`, `dosis`)
-            VALUES ('" . $last_id . "','" .  $obat_id . "',  '" . $jumlah . "', '" . $dosis . "')";
-            $stmt2 = $con->prepare($sql2);
-            $stmt2->execute();
-            if ($stmt2->affected_rows > 0) {
-                echo 'sql 2 success' . "\n id input: $last_id \n ";
-                $arr = ["result" => "success", "data" => $obat_array];
-            } else {
-                $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
-            }
+            $arr = ["result" => "success",  "resep_id" => $last_id, "data" => $obat_array];
         } else {
             $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
         }
