a05dc3e93100401dbc1cee2c439d29d054577131
mlatifr <mlatifraldi@gmail.com>
2021-10-04 05:26:22 +0700

    aptkr inpt resep:next aptkr input dosis&obat

--- apoteker_input_resep.php
+++ apoteker_input_resep.php
@@ -4,28 +4,36 @@
 $arr = [];
 $last_id;
 extract($_POST);
-foreach ($obat_array as $row => $value) {
-    $visit_id = mysqli_real_escape_string($con, $value['visit_id']);
-    $nama_pembeli = mysqli_real_escape_string($con, $value['nama_pembeli']);
-    $user_id_apoteker = mysqli_real_escape_string($con, $value['user_id_apoteker']);
-    $tgl_penulisan_resep = mysqli_real_escape_string($con, $value['tgl_penulisan_resep']);
-    //pelayanan resep dari pasien yg visit
-    if ($visit_id) {
-        $sql = "INSERT INTO `resep_apoteker` ( `visit_id`, `nama_pembeli`, `user_id_apoteker`, `tgl_penulisan_resep`)
-        VALUES ('" . $visit_id . "','" .  $nama_pembeli . "',  '" . $user_id_apoteker . "', '" . $tgl_penulisan_resep . "')";
-        $stmt = $con->prepare($sql);
-        $stmt->execute();
-        if ($stmt->affected_rows > 0) {
-            $last_id = $con->insert_id;
-            $arr = ["result" => "success",  "resep_id" => $last_id, "data" => $obat_array];
-        } else {
-            $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
-        }
+// $visit_id = mysqli_real_escape_string($con, $value['visit_id']);
+// $nama_pembeli = mysqli_real_escape_string($con, $value['nama_pembeli']);
+// $user_id_apoteker = mysqli_real_escape_string($con, $value['user_id_apoteker']);
+// $tgl_penulisan_resep = mysqli_real_escape_string($con, $value['tgl_penulisan_resep']);
+//pelayanan resep dari pasien yg visit
+if ($visit_id) {
+    $sql =
+        "INSERT INTO `resep_apoteker` ( `visit_id`, `user_id_apoteker`, `tgl_penulisan_resep`)
+        VALUES ('" . $visit_id  . "','" . $user_id_apoteker . "','" . $tgl_penulisan_resep . "')";
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+    if ($stmt->affected_rows > 0) {
+        $last_id = $con->insert_id;
+        $arr = ["result" => "success",  "resep_id" => $last_id];
+    } else {
+        $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
     }
-    // pelayanan resep dari pembeli non-visit
-    elseif ($nama_pembeli) {
-        $sql = "INSERT INTO `resep_apoteker` (`nama_pembeli`, `user_id_apoteker`, `obat_id`, `dosis`, `jumlah`)
-        VALUES ('" .  $nama_pembeli . "',  '" . $user_id_apoteker . "', '" . $obat_id . "', '" . $dosis . "', '" . $jumlah . "')";
+}
+// pelayanan resep dari pembeli non-visit
+elseif ($nama_pembeli) {
+    $sql =
+        "INSERT INTO `resep_apoteker` (`nama_pembeli`, `user_id_apoteker`, `tgl_penulisan_resep`)
+        VALUES ('" .  $nama_pembeli . "',  '" . $user_id_apoteker . "', '" . $tgl_penulisan_resep . "')";
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+    if ($stmt->affected_rows > 0) {
+        $last_id = $con->insert_id;
+        $arr = ["result" => "success",  "resep_id" => $last_id];
+    } else {
+        $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($obat_array)];
     }
 };
 
