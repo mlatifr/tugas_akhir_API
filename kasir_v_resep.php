c747c78dee2a2ee0d3b0453e58c181fab070529e
mlatifr <mlatifraldi@gmail.com>
2021-10-04 05:31:52 +0700

    kasir dpt melihat list obat yg dibeli  pasien

--- kasir_v_resep.php
+++ kasir_v_resep.php
@@ -5,53 +5,50 @@ require 'connect.php';
 ?>
 <?php
 extract($_POST);
-$nama_pembeli = "%{$_POST['nama_pembeli']}%";
+$nama_pembeli = "{$_POST['nama_pembeli']}";
 // $user_id = "%{$_POST['user_id']}%";
 // $visit_id = "%{$_POST['visit_id']}%";
 // echo $user_id . ' ' . $tgl_visit;
 // echo ' ' . $tgl_visit;
-if ($visit_id != null) {
+if ($visit_id) {
     $sql = "SELECT 
-                resep_apoteker.visit_id,
-                -- user_id_apoteker as usr_id_aptkr,
-                nama_pembeli,
-                obat_id,
-                -- dosis,
+                ra.id as resep_id,
+                ra.visit_id as visit_id,
+                ra.nama_pembeli,
+                ra.user_id_apoteker,
+                ra.tgl_penulisan_resep as tgl_resep,
+                raho.obat_id as obat_id,
                 jumlah,
-                -- resep.id as resep_id,
-                -- visit_id,
-                -- obat.id as id_obat,
-                nama as nama_obat,
-                -- stok,
-                kadaluarsa,
-                harga_jual
-                -- harga_beli
-            FROM resep_apoteker 
-            INNER JOIN obat ON resep_apoteker.obat_id=obat.id 
-            WHERE visit_id = ? 
-            ORDER BY `obat`.`nama` ASC";
+                dosis,
+                obt.nama,
+                obt.stok,
+                obt.kadaluarsa,
+                obt.harga_jual
+            FROM resep_apoteker ra
+            INNER JOIN rsp_aptkr_has_obat raho ON ra.id=raho.resep_apoteker_id
+            INNER JOIN obat obt ON raho.obat_id=obt.id
+            where visit_id = ?
+            ORDER BY `obt`.`nama` ASC";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("s", $visit_id);
-} elseif ($nama_pembeli != null) {
+} elseif ($nama_pembeli) {
     $sql = "SELECT 
-                -- resep_apoteker.visit_id,
-                -- user_id_apoteker as usr_id_aptkr,
-                nama_pembeli,
-                obat_id,
-                -- dosis,
+                ra.id as resep_id,
+                ra.nama_pembeli,
+                ra.user_id_apoteker,
+                ra.tgl_penulisan_resep as tgl_resep,
+                raho.obat_id as obat_id,
                 jumlah,
-                -- resep.id as resep_id,
-                -- visit_id,
-                -- obat.id as id_obat,
-                nama as nama_obat,
-                -- stok,
-                kadaluarsa,
-                harga_jual
-                -- harga_beli
-            FROM resep_apoteker 
-            INNER JOIN obat ON resep_apoteker.obat_id=obat.id 
-            WHERE nama_pembeli like ? 
-            ORDER BY `obat`.`nama` ASC";
+                dosis,
+                obt.nama,
+                obt.stok,
+                obt.kadaluarsa,
+                obt.harga_jual
+            FROM resep_apoteker ra
+            INNER JOIN rsp_aptkr_has_obat raho ON ra.id=raho.resep_apoteker_id
+            INNER JOIN obat obt ON raho.obat_id=obt.id
+            WHERE nama_pembeli like ?
+            ORDER BY `obt`.`nama` ASC";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("s", $nama_pembeli);
 };
