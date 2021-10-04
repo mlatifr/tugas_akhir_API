86b4301791af2aa678c858a3cafb7a1903f44706
mlatifr <mlatifraldi@gmail.com>
2021-10-04 14:01:29 +0700

    kasir: mengurangi stok obat ketika tjual

--- kasir_updt_stok.php
+++ kasir_updt_stok.php
@@ -8,14 +8,12 @@ $terjual;
 extract($_POST);
 foreach ($obat_array as $row => $value) {
     $obat_id = mysqli_real_escape_string($con, $value['obat_id']);
-    // $stok_baru = mysqli_real_escape_string($con, $value['stok_baru']);
-    // $stok_lama = mysqli_real_escape_string($con, $value['stok_lama']);
+    $stok_lama = mysqli_real_escape_string($con, $value['stok_lama']);
     $terjual = mysqli_real_escape_string($con, $value['terjual']);
-
-    // $stok_baru = $stok_lama - $terjual;
+    $stok_baru = $stok_lama - $terjual;
     $sql2 = "UPDATE `obat` SET `stok` = ? WHERE `obat`.`id` = ?";
     $stmt2 = $con->prepare($sql2);
-    $stmt2->bind_param("ss", $terjual, $obat_id);
+    $stmt2->bind_param("ss", $stok_baru, $obat_id);
     $stmt2->execute();
     if ($stmt2->affected_rows > 0) {
         $arr2 = ["result" => "success", "data" => $obat_array];
