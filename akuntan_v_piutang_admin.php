4c021fc08b564de3e879f6cb403cb14167795949
mlatifr <mlatifraldi@gmail.com>
2021-10-13 01:41:22 +0700

    akuntanVpiutangAAdminJasmed: next akuntan V hutang

---
+++ akuntan_v_piutang_admin.php
@@ -0,0 +1,38 @@
+<?php require 'connect.php';
+?>
+<?php
+$arr = [];
+$data = [];
+if (isset($_POST['tgl_transaksi'])) {
+    $tgl_transaksi = "%{$_POST['tgl_transaksi']}%";
+    $sql =
+        "SELECT DATE_FORMAT(np.tgl_transaksi,'%Y-%m') AS periode_transaksi, SUM(np.biaya_admin) as total_admin, vst.perusahaan
+        FROM nota_penjualan np
+        INNER JOIN visit vst ON vst.id=np.visit_id
+        WHERE np.tgl_transaksi LIKE ?
+        && vst.perusahaan IS NOT NULL";
+}
+if (isset($_POST['tgl_transaksi_detail'])) {
+    $tgl_transaksi = "%{$_POST['tgl_transaksi_detail']}%";
+    $sql =
+        "SELECT DATE_FORMAT(np.tgl_transaksi,'%Y-%m') AS periode_transaksi, (np.biaya_admin) as total_admin, vst.perusahaan
+        FROM nota_penjualan np
+        INNER JOIN visit vst ON vst.id=np.visit_id
+        WHERE np.tgl_transaksi LIKE ?
+        && vst.perusahaan IS NOT NULL";
+}
+$stmt = $con->prepare($sql);
+$stmt->bind_param("s", $tgl_transaksi);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    while ($r = mysqli_fetch_assoc($result)) {
+        array_push($data, $r);
+    }
+    $arr = ["result" => "success", "data" => $data];
+} else {
+    $arr = ["result" => "error", "message" => "sql error: $sql"];
+}
+echo json_encode($arr);
+$con->close();
+?>
