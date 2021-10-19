0dae6d408a0b7963b986ba9a7de7395bcaacd21b
mlatifr <mlatifraldi@gmail.com>
2021-10-20 05:16:51 +0700

    backup database: xampp habis error

--- klinik.sql
+++ klinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2021 at 04:44 AM
+-- Generation Time: Oct 20, 2021 at 12:16 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
