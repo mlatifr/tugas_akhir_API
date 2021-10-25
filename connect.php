a7f8672651be594a631dc96b85ee80ce42eac866
mlatifr <mlatifraldi@gmail.com>
2021-10-26 01:46:54 +0700

    header("Access-Control-Allow-Origin: *");

--- connect.php
+++ connect.php
@@ -1,5 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Accept");
+header("Access-Control-Allow-Credentials=true");
+header("Access-Control-Allow-Headers=Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale");
+header("Access-Control-Allow-Methods=POST, OPTIONS");
 $arr = null;
 $con = new mysqli("localhost", "root", "", "klinik");
 if ($con->connect_error) {
@@ -7,3 +11,4 @@ if ($con->connect_error) {
     echo json_encode($arr);
     die();
 }
+?>
