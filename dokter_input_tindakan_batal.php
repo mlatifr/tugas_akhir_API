a49bf5480dd32cb1ff95e11a6384479831f7ae77
mlatifr <mlatifraldi@gmail.com>
2021-10-28 17:15:26 +0700

    DrBtlInputTIndakan:Berhasil

--- dokter_input_tindakan_batal.php
+++ dokter_input_tindakan_batal.php
@@ -1,18 +1,18 @@
 <?php require 'connect.php';
 ?>
 <?php
-$tindakan_id = "{$_POST['tindakan_id']}";
-$visit_id = "{$_POST['tindakan_id']}";
-$mt_sisi = "{$_POST['mt_sisi']}";
-// extract($_POST);
+// $tindakan_id = "{$_POST['tindakan_id']}";
+// $visit_id = "{$_POST['tindakan_id']}";
+// $mt_sisi = "%{$_POST['mt_sisi']}%";
+extract($_POST);
 $sql = "DELETE 
-        FROM `visit_has_tindakan` 
-        WHERE tindakan_id=$tindakan_id
-        AND visit_id =$visit_id
-        AND mt_sisi= '$mt_sisi'";
+FROM `visit_has_tindakan` 
+WHERE tindakan_id=?
+AND visit_id =?
+AND mt_sisi LIKE ?";
 $stmt = $con->prepare($sql);
+$stmt->bind_param("sss", $tindakan_id, $visit_id, $mt_sisi);
 $stmt->execute();
-
 $arr = [];
 $data = [];
 if ($stmt->affected_rows > 0) {
@@ -20,7 +20,7 @@ if ($stmt->affected_rows > 0) {
     // $arr = ["result" => "success", 'jumlah array' => count($tindakan_array), "data" => $tindakan_array];
     // $arr = ["data" => $tindakan_array];
 } else {
-    $arr = ["result" => "fail", "Error" => $con->error];
+    $arr = ["result" => "fail", 'sql' => $sql, "Error" => $con->error];
 }
 echo json_encode($arr);
 $con->close();
