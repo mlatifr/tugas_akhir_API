71fba3b0d0334e31663578c6c233cc79f7eb09bf
mlatifr <mlatifraldi@gmail.com>
2021-10-31 04:58:49 +0700

    DrVKeranjangRekamObtVsit

---
+++ dokter_v_keranjang_resep_obat.php
@@ -0,0 +1,26 @@
+<?php require 'connect.php';
+?>
+<?php
+$visit_id = "{$_POST['visit_id']}";
+$sql =
+    "SELECT obt.nama,rho.jumlah,rho.dosis 
+    FROM resep_has_obat rho
+    INNER JOIN obat obt ON rho.obat_id=obt.id
+    WHERE visit_id = ?
+";
+$stmt = $con->prepare($sql);
+$stmt->bind_param("s", $visit_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$data = [];
+if ($result->num_rows > 0) {
+    while ($r = mysqli_fetch_assoc($result)) {
+        array_push($data, $r);
+    }
+    $arr = ["result" => "success", "data" => $data];
+} else {
+    $arr = ["result" => "error", "data" => 'tidak ditemukan', "message" => "sql error: $sql"];
+}
+echo json_encode($arr);
+$stmt->close();
+$con->close();
