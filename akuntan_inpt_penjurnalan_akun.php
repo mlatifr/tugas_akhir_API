d79a2380ffb6213e209ef8a4303a4ac4c6b55a57
mlatifr <mlatifraldi@gmail.com>
2021-11-06 12:49:22 +0700

    PjrnlnHsAkun:InsertSatu_satu

--- akuntan_inpt_penjurnalan_akun.php
+++ akuntan_inpt_penjurnalan_akun.php
@@ -3,26 +3,24 @@
 <?php
 $arr = [];
 extract($_POST);
-foreach ($transaksi_array as $row => $value) {
-    $penjurnalan_id = mysqli_real_escape_string($con, $value['penjurnalan_id']);
-    $daftar_akun_id = mysqli_real_escape_string($con, $value['daftar_akun_id']);
-    $tgl_catat = mysqli_real_escape_string($con, $value['tgl_catat']);
-    $debet = mysqli_real_escape_string($con, $value['debet']);
-    $kredit = mysqli_real_escape_string($con, $value['kredit']);
-    $ket_transaksi = mysqli_real_escape_string($con, $value['ket_transaksi']);
-    $sql =
-        "INSERT INTO `penjurnalan_has_akun` 
+$penjurnalan_id = ($_POST['penjurnalan_id']);
+$daftar_akun_id = $_POST['daftar_akun_id'];
+$tgl_catat = $_POST['tgl_catat'];
+$debet = $_POST['debet'];
+$kredit = $_POST['kredit'];
+$ket_transaksi = $_POST['ket_transaksi'];
+$sql =
+    "INSERT INTO `penjurnalan_has_akun` 
         (`penjurnalan_id`, `daftar_akun_id`, `tgl_catat`, `debet`, `kredit`, `ket_transaksi`)  
         VALUES 
         ('" . $penjurnalan_id . "','" . $daftar_akun_id . "','" . $tgl_catat . "','" . $debet . "','" . $kredit . "','" . $ket_transaksi . "')";
-    $stmt = $con->prepare($sql);
-    $stmt->execute();
+$stmt = $con->prepare($sql);
+$stmt->execute();
 
-    if ($stmt->affected_rows > 0) {
-        $arr = ["result" => "success", "data" => $transaksi_array];
-    } else {
-        $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($transaksi_array)];
-    }
+if ($stmt->affected_rows > 0) {
+    $arr = ["result" => "success", "penjurnalan_has_akun_id" => $con->insert_id];
+} else {
+    $arr = ["result" => "fail", "Error" => $con->error, 'jumlah_array' => count($transaksi_array)];
 };
 echo json_encode($arr);
 
