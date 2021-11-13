2d61f900a27bf4c72458ffa2ad2ca444c78e51a3
mlatifr <mlatifraldi@gmail.com>
2021-11-13 10:34:32 +0700

    pasienDaftarUsernameBaru

--- pasien_daftar_user_baru.php
+++ pasien_daftar_user_baru.php
@@ -1,70 +1,33 @@
 <?php require 'connect.php';
 ?>
 <?php
-$user_klinik_id;
+$user_klinik_id = '';
+$data = [];
 extract($_POST);
 $sql =
-    "IINSERT INTO `user_klinik` ( `username`, `sandi`) 
-VALUES ( ?,?)";
+    "INSERT INTO `user_klinik` ( `username`, `sandi`) 
+VALUES (?,?)";
 $stmt = $con->prepare($sql);
 $stmt->bind_param("ss", $username, $sandi);
 $stmt->execute();
 if ($stmt->affected_rows > 0) {
     echo "sql 1 success \n";
-    $user_klinik_id = $con->insert_id;
-    $sql2 = "
-    INSERT INTO `pasien` 
-    ( 
-    `user_klinik_id`, 
-    `no_rekam_medis`, 
-    `NIK`, 
-    `nama`,
-    `tempat_lahir`, 
-    `tgl_lahir`, 
-    `kelamin`,
-    `golongan_darah`, 
-    `alamat`, 
-    `agama`, 
-    `status_kawin`, 
-    `pekerjaan`, 
-    `kewarganegaraan`, 
-    `tlp`,
-    )
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,)";
+    $user_klinik_id = "$con->insert_id";
+    // echo $user_klinik_id;
+    $sql2 =
+        "INSERT INTO `pasien` 
+    (`user_klinik_id`, `no_rekam_medis`, `NIK`, `nama`, `tempat_lahir`, `tgl_lahir`, `kelamin`, `golongan_darah`, `alamat`, `agama`, `status_kawin`, `pekerjaan`, `kewarganegaraan`, `tlp`, `hp`, `created`)
+    VALUES 
+    (?,?,?,?,?,?,?,?,?,?,?,?,?,?, NULL, NULL)";
     $stmt2 = $con->prepare($sql2);
-    $stmt2->bind_param(
-        "sssssssssssssss",
-        $user_klinik_id,
-        $user_klinik_id,
-        $user_klinik_id,
-        $NIK,
-        $nama,
-        $tempat_lahir,
-        $tgl_lahir,
-        $kelamin,
-        $golongan_darah,
-        $alamat,
-        $agama,
-        $status_kawin,
-        $pekerjaan,
-        $kewarganegaraan,
-        $tlp
-    );
+    $stmt2->bind_param("ssssssssssssss", $user_klinik_id, $user_klinik_id, $NIK, $nama, $tempat_lahir, $tgl_lahir, $kelamin, $golongan_darah, $alamat, $agama, $status_kawin, $pekerjaan, $kewarganegaraan, $tlp);
     $stmt2->execute();
     if ($stmt2->affected_rows > 0) {
         // $no_antrean = $no_antrean + 1;
-        // echo "sql 2 success \n";
-        $sql3 = "UPDATE antrean_admin SET antrean_terakhir = ? WHERE antrean_admin.id=1";
-        $stmt3 = $con->prepare($sql3);
-        $stmt3->bind_param("s", $no_antrean);
-        $stmt3->execute();
+        echo "sql 2 success \n";
         $arr = [
             "result" => "success",
-            "visit_id" => $visit_id,
-            "user_klinik_id" => $user_klinik_id,
-            "keluhan" => $keluhan,
-            'no_antrean' => $no_antrean,
-            'tgl_visit' => date('Y-m-d H:i:s')
+            "data" => $username,
         ];
     }
 } else {
