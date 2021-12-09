ce44fa4e7633d006e7a18b1645904ddfee2c5ee7
mlatifr <mlatifraldi@gmail.com>
2021-12-10 05:49:25 +0700

    pemilik send krjg obat to DB with status order

--- pemilik_input_order_item.php
+++ pemilik_input_order_item.php
@@ -6,13 +6,15 @@ $order_obat_id = '';
 $jumlah_order = '';
 $nama = '';
 $harga_beli = '';
+$harga_jual = '';
 $obat_id = '';
+$status_order = '';
 extract($_POST);
 $sql =
     "INSERT INTO `obat` 
-        (`order_obat_id`, `jumlah_order`, `nama`, `harga_beli`)
+        (`order_obat_id`, `jumlah_order`, `nama`, `harga_beli`, `harga_jual`,`status_order`)
         VALUES 
-        ('" . $order_obat_id . "','" .  $jumlah_order . "','" . $nama . "', '" . $harga_beli . "')";
+        ('" . $order_obat_id . "','" .  $jumlah_order . "','" . $nama . "', '" . $harga_beli . "', '" . $harga_jual . "', '" . $status_order . "')";
 $stmt = $con->prepare($sql);
 $stmt->execute();
 $obat_array = $con->insert_id;
