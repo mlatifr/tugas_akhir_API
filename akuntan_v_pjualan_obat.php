80da7206e7f6ad6f269422a365710589684b741a
mlatifr <mlatifraldi@gmail.com>
2021-12-22 20:49:18 +0700

    akuntan v nota penjualan

--- akuntan_v_pjualan_obat.php
+++ akuntan_v_pjualan_obat.php
@@ -21,22 +21,19 @@ if (isset($_POST['tgl_resep_detail'])) {
     $stmt->bind_param("s", $tgl_resep_detail);
 }
 // kalau pembeli obat merupakan pasien yg visit
-elseif (isset($_POST['tgl_resep_visit'])) {
-    $tgl_resep_visit = "%{$_POST['tgl_resep_visit']}%";
+elseif (isset($_POST['tgl_resep_nota'])) {
+    $tgl_resep_nota = "%{$_POST['tgl_resep_nota']}%";
     $sql =
         "SELECT 
-        SUM(raho.jumlah*obt.harga_jual) as total_harga, 
-        ra.tgl_penulisan_resep as tgl_resep
-        FROM rsp_aptkr_has_obat raho
-        INNER JOIN obat obt ON obt.id=raho.obat_id
-        INNER JOIN resep_apoteker ra ON ra.id=raho.resep_apoteker_id 
-        INNER JOIN visit vst ON ra.visit_id=vst.id
-        WHERE ra.tgl_penulisan_resep LIKE ?
-        &&  vst.perusahaan IS NULL";
+        nota_penjualan.id as nota_id,
+        nota_penjualan.user_id as user_kasir,
+        nota_penjualan.visit_id as visit_id
+        FROM `nota_penjualan` 
+        WHERE tgl_transaksi LIKE ?";
     $stmt = $con->prepare($sql);
-    $stmt->bind_param("s", $tgl_resep_visit);
-} elseif (isset($_POST['tgl_resep_non_visit'])) {
-    $tgl_resep_non_visit = "%{$_POST['tgl_resep_non_visit']}%";
+    $stmt->bind_param("s", $tgl_resep_nota);
+} elseif (isset($_POST['tgl_penjualan'])) {
+    $tgl_penjualan = "%{$_POST['tgl_penjualan']}%";
     $sql =
         "SELECT 
         np.tgl_transaksi as tgl_transaksi,
@@ -55,7 +52,7 @@ elseif (isset($_POST['tgl_resep_visit'])) {
         ORDER BY `np`.`tgl_transaksi` ASC;
         ";
     $stmt = $con->prepare($sql);
-    $stmt->bind_param("s", $tgl_resep_non_visit);
+    $stmt->bind_param("s", $tgl_penjualan);
 }
 $stmt->execute();
 $result = $stmt->get_result();
