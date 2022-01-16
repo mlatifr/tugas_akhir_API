bdc17f58576d472a7aa14bc8532bf7b3a07b60d3
mlatifr <mlatifraldi@gmail.com>
2022-01-17 05:59:16 +0700

    LabaRugi JumlahNota HPP

--- akuntan_v_pjualan_obat_hpp.php
+++ akuntan_v_pjualan_obat_hpp.php
@@ -6,15 +6,22 @@ $data = [];
 if (isset($_POST['tgl_list_nota_hpp'])) {
     $tgl_list_nota_hpp = "%{$_POST['tgl_list_nota_hpp']}%";
     $sql =
-        "SELECT 
-        np.id as no_nota,
-        (raho.jumlah*obt.harga_beli)as total
-        FROM rsp_aptkr_has_obat raho
-        INNER JOIN obat obt ON obt.id=raho.obat_id
-        INNER JOIN resep_apoteker ra ON ra.id=raho.resep_apoteker_id 
-        INNER JOIN nota_penjualan np ON np.visit_id=ra.visit_id 
-        WHERE np.tgl_transaksi LIKE ?
-        ORDER BY np.id ASC";
+        "SELECT
+        np.id AS nota_id,
+        np.tgl_transaksi,
+        SUM(raho.jumlah * obt.harga_beli) AS total_penjualan
+    FROM
+        nota_penjualan np
+    INNER JOIN resep_apoteker ra ON
+        np.resep_apoteker_id = ra.id
+    INNER JOIN rsp_aptkr_has_obat raho ON
+        raho.resep_apoteker_id = ra.id
+    INNER JOIN obat obt ON
+        obt.id = raho.obat_id
+    WHERE
+        np.tgl_transaksi LIKE ?
+    GROUP BY
+        np.id";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("s", $tgl_list_nota_hpp);
 }
@@ -23,16 +30,33 @@ elseif (isset($_POST['tgl_list'])) {
     //list nota untuk hpp
     $tgl_list = "%{$_POST['tgl_list']}%";
     $sql =
-        "SELECT 
-        SUM(raho.jumlah*obt.harga_beli) as total_harga, 
-        ra.tgl_penulisan_resep as tgl_resep
-        FROM rsp_aptkr_has_obat raho
-        INNER JOIN obat obt ON obt.id=raho.obat_id
-        INNER JOIN resep_apoteker ra ON ra.id=raho.resep_apoteker_id 
-        INNER JOIN visit vst ON ra.visit_id=vst.id
-        INNER JOIN nota_penjualan np ON np.resep_apoteker_id=ra.id
-        WHERE ra.tgl_penulisan_resep LIKE ?
-        -- &&  vst.perusahaan IS NULL
+        "SELECT
+        np.tgl_transaksi AS tgl_transaksi,
+        SUM((raho.jumlah * obt.harga_beli)) AS total_harga,
+        (
+        SELECT
+            COUNT(nota_penjualan.id)
+        FROM
+            `nota_penjualan`
+        INNER JOIN resep_apoteker ra ON
+            nota_penjualan.resep_apoteker_id = ra.id
+        WHERE
+            nota_penjualan.tgl_transaksi LIKE np.tgl_transaksi AND ra.id IS NOT NULL
+        ) AS total_nota
+        FROM
+            nota_penjualan np
+        INNER JOIN resep_apoteker ra ON
+            np.resep_apoteker_id = ra.id
+        INNER JOIN rsp_aptkr_has_obat raho ON
+            raho.resep_apoteker_id = ra.id
+        INNER JOIN obat obt ON
+            obt.id = raho.obat_id
+        WHERE
+            np.tgl_transaksi LIKE ?
+        GROUP BY
+            np.tgl_transaksi
+        ORDER BY
+            `np`.`tgl_transaksi` ASC
         ";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("s", $tgl_list);
