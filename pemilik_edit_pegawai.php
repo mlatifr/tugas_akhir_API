f00bd986967b586811f5b28d2eba371aa0a7d2fe
mlatifr <mlatifraldi@.com>
2022-07-03 06:53:26 +0700

    edit pegawai

--- pemilik_edit_pegawai.php
+++ pemilik_edit_pegawai.php
@@ -11,27 +11,30 @@ $status =  isset($_POST['status']) ? $_POST['status'] : '';
 $unit_kerja =  isset($_POST['unit_kerja']) ? $_POST['unit_kerja'] : '';
 $tlp = isset($_POST['tlp']) ? $_POST['tlp'] : '';
 $alamat = isset($_POST['alamat']) ? $_POST['alamat'] : '';
-if (isset($_POST['id_pegawai']) && isset($_POST['status']) && empty(($_POST['nama']))) {
-    $sql =
-        "UPDATE `info_pegawai` SET `status` = '$status' WHERE `info_pegawai`.`id` =$id_pegawai
-        ";
-}
+$sql = "";
+// if (isset($_POST['id_pegawai']) && isset($_POST['status']) && empty(($_POST['nama']))) {
+//     $sql =
+//         "UPDATE `info_pegawai` SET `status` = '$status' WHERE `info_pegawai`.`id` =$id_pegawai
+//         ";
+// }
 //update data pegawai
-if (isset($_POST['id_pegawai']) && isset($_POST['nama']) && isset($_POST['tlp']) && isset($_POST['alamat']) && isset($_POST['unit_kerja'])) {
+if (isset($_POST['id_pegawai']) && isset($_POST['status']) && isset($_POST['nama']) && isset($_POST['tlp']) && isset($_POST['alamat']) && isset($_POST['unit_kerja'])) {
     $sql =
         "UPDATE
-        `info_pegawai`
-    SET
-        nama = '$nama', 
-        unit_kerja = '$unit_kerja',
-        tlp = '$tlp',
-        alamat = '$alamat'
-    WHERE
-        info_pegawai.id = $id_pegawai
+            `info_pegawai`
+        SET
+            `nama` = '$nama',
+            `status` = '$status',
+            `unit_kerja` = '$unit_kerja',
+            `tlp` = '1$tlp',
+            `alamat` = '$alamat'
+        WHERE
+            `info_pegawai`.`id` = $id_pegawai
     ";
+    // echo "sql: " . $sql;
 }
 if ($con->query($sql) === TRUE) {
-    $arr = ["result" => "success yes", "data" => $sql];
+    $arr = ["result" => "success", "data" => $nama . ' ' . $status . ' ' . $unit_kerja . ' ' . $tlp . ' ' . $alamat];
 } else {
     $arr = ["result" => "error", "message" => "sql error: $sql"];
 }
